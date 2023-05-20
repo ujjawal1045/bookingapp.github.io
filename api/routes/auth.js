3ca7d1eb02a12b1067b7e4cecb9af21ef012dc00
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2023-05-20 16:16:41 +0530

    created routes and middle ware and rendered rthem

---
+++ api/routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("hello, this is auth endpoint");
+})
+
+router.get("/register", (req, res) => {
+    res.send("hello, this is auth register endpoint");
+})
+module.exports = router;
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+
+const authRoute = require('./api/routes/auth.js');
+const usersRoute = require('./api/routes/users.js');
+const hotelRoute = require('./api/routes/hotels.js');
+const roomRoute = require('./api/routes/rooms.js');
 const app = express();
 
 const port = 8000;
@@ -7,6 +12,18 @@ const port = 8000;
 //adding mongoose databse
 
 const db = require('./config/mongoose');
+
+//api request
+app.get("/", (req, res) => {
+    res.send('hello first request');
+})
+
+//middleware
+app.use("/api/auth", authRoute);
+app.use("/api/users",usersRoute);
+app.use("/api/rooms",roomRoute);
+app.use("/api/hotels",hotelRoute);
+
 
 app.listen(port, function(err) {
     if(err){
