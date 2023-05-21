89072ed67ffbfd68b5d6bbb2c73873208345c29d
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2023-05-21 13:13:09 +0530

    user login authenticated

--- api/controller/auth_controller.js
+++ api/controller/auth_controller.js
@@ -17,3 +17,22 @@ module.exports.register = async (req, res, next) => {
         next(err);
     }
 }
+
+//login
+module.exports.login = async (req, res, next) => {
+    try {
+       const user = await User.findOne({username: req.body.username});
+       if(!user) 
+       return res.status(404).send("user not found");
+
+       const isPasswordCorrect = await bcrypt.compare(req.body.password,user.password);
+       if(!isPasswordCorrect)
+       return res.status(400).send("username or password incorrect");
+
+       //hiding password from displaying
+       const{password, ...otherDetails} = user._doc;
+        res.status(200).json({...otherDetails});
+    } catch (err) {
+        next(err);
+    }
+}
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,4 +4,5 @@ const router = express.Router();
 
 
 router.post("/register", authController.register);
+router.post("/login", authController.login );
 module.exports = router;
