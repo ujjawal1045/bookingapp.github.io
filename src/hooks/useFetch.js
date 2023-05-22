e21af63a83c83de43cbd8435a367eb9787c65837
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2023-05-22 17:25:04 +0530

    handled the landing page of hotel

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -22,7 +22,7 @@ const useFetch = (url) => {
             setLoading(false);
         };
         fetchData();
-    }, []);
+    }, [url]);
 
     const reFetch = async ()=>{
         setLoading(true)
