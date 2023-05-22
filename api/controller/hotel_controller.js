2e9c9e65f048e9cfa38b593719396126e7a38412
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2023-05-22 14:16:26 +0530

    rendered the hotel detail by city type property type and guest recommended type

--- api/controller/hotel_controller.js
+++ api/controller/hotel_controller.js
@@ -1,4 +1,5 @@
 const Hotel = require('../models/Hotel');
+const qs = require('qs');
 module.exports.createHotel = async  (req, res, next) => {
     const newHotel = new Hotel(req.body);
     try {
@@ -40,19 +41,54 @@ module.exports.getHotel = async (req, res, next) => {
     }
 }
 
+// module.exports.getAllHotel = async (req, res, next) => {
+//     const{min, max, ...others} = req.query;
+//     try {
+//         let limit = parseInt(req.query.limit) || 4;
+//         const hotels = await Hotel.find({
+//             ...others,
+//              cheapestPrice: {$gt: min || 1, $lt: max || 999},}).limit(parseInt(limit));
+//         res.status(200).json(hotels);
+//     } catch(err) {
+//         next(err);
+//     }
+// }
+
 module.exports.getAllHotel = async (req, res, next) => {
+    const query = qs.parse(req.url.split('?')[1]);
+    const { featured, min, max } = query;
     try {
-        const hotels = await Hotel.find();
+        let limit = parseInt(query.limit) || 4;
+        const queryObject = {};
+        if (featured) {
+            queryObject.featured = featured === 'true';
+        }
+        if (min && max) {
+            queryObject.cheapestPrice = { $gt: parseInt(min), $lt: parseInt(max) };
+        }
+        const hotels = await Hotel.find(queryObject).limit(limit);
         res.status(200).json(hotels);
-    } catch(err) {
+    } catch (err) {
         next(err);
     }
 }
 
+
 module.exports.CountByType = async (req, res, next) => {
     try {
-        const hotels = await Hotel.find();
-        res.status(200).json(hotels);
+        const hotelCount = await Hotel.countDocuments({typee:"hotel"}) ;
+        const apartementCount =  await Hotel.countDocuments({typee:"Apartement"});
+        const oyoCount =await Hotel.countDocuments({typee: "oyo hotel"});
+        const lodgeCount =await Hotel.countDocuments({typee:"lodge"});
+        const resortCount =await Hotel.countDocuments({typee:"resort"});
+      
+        res.status(200).json([
+            {typee:"hotel", count : hotelCount},
+            {typee: "resort", count: resortCount},
+            {typee:"apartement", count : apartementCount},
+            {typee:"oyo hotel", count : oyoCount},
+            {typee:"lodge", count: lodgeCount}
+        ]);
     } catch(err) {
         next(err);
     }
